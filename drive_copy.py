e5b855de8083c3958a9746845f75712ec7797c19
Matthew Craig <mattwcraig@gmail.com>
2018-12-07 23:18:00 -0700

    Add short script to execute the copying

---
+++ drive_copy.py
@@ -0,0 +1,29 @@
+import os
+
+from ruamel import yaml
+from binstar_client import Conflict
+
+from sync import sync
+
+FROM = 'conda-forge'
+TO = 'astropy'
+
+
+def main(requirements_path):
+    with open(requirements_path, 'rt') as f:
+        package_list = yaml.safe_load(f)
+
+    token = os.getenv('BINSTAR_TOKEN')
+
+    print('token is', token)
+
+    for package in package_list:
+        print(package['name'])
+        try:
+            sync(FROM, TO, package['name'], token, to_label='main')
+        except Conflict:
+            print('Skipping because of a conflict anaconda.org')
+
+
+if __name__ == '__main__':
+    main('requirements.yml')
